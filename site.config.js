0650d4cf6b162ad9c89019707b81765bfb56189e
liuwei <roostinghawk@163.com>
2023-01-04 15:42:10 +0800

    url update

--- site.config.js
+++ site.config.js
@@ -50,25 +50,7 @@ module.exports = {
   // example:
   //
   pageUrlOverrides: {
-    '/os': 'ff8a4877c04f42eda0d050547c117ea5',
-    '/os-introduction': '68390e1112d749debb748387a82ebd9f',
-    '/create-notion-blog': 'Notion-40362e4b8973491bbbc147237f1e9227',
-    '/create-vuepress-blog': 'vuepress-github-pagegitee-e4c85fe845104c5aa004a19df445f29e',
-    '/c++': 'c-6cfc812927c44a688e874d6212265b30',
-    '/java': 'java-a0975c0ad4fb473ca8c2a803a7813a4d',
-    '/i-am-cai-bing-tong-xue': '8f65e44b7a5a47788fdcc8dde9522bfa',
-    '/redis': 'redis-df0a05f97c9a462e83a3ecf5d0066578',
-    '/common-question': '8ea9f59839d04750b08fa7e13045ab08',
-    '/how-git-work': 'Git-c3260ebf0ad04d9d8d2081bf1f3bf2b8',
-    '/algorithm-sort': '7f7769466b5847dab94716dc0d9c1ac4',
-    '/algorithm-sort-js' : 'JavaScript-f5b50f39a66645ec97dcd409497ed2a9',
-    '/algorithm-binary-tree-traversal' :'07df0ddf43664a758fb4725070b71854',
-    '/network': '926b875211b44f978d39200f8d39ef1d',
-    '/database': '337b1694b9344af9a424136b87827ad3',
-    '/advanced-algorithm': '854a54c0567944488842e3f01faf6ab3',
-    '/frontend': '83dc1ec0083a4bdfbe7d3ac6a5b07664',
-    '/post-list': '651765de6a0f4a729f9186f26381aef3',
-    '/one-year-review': 'c6461186bea149eb9a83c0d7284f2921',
+    '/running': 'fcb5f7f7b1db4954b7e057d8ed7c7237',
   },
   showPageAsideSocials: true,
   footCounterList: [
